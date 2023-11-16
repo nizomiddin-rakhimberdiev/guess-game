458b1f316c245b12bd45679e008a104f597fec15
nizomiddin-rakhimberdiev <nizomiddinoff@mail.ru>
2023-11-16 19:41:47 +0500

    changed game settings

--- guess.py
+++ guess.py
@@ -1,9 +1,9 @@
 import random
 
 n = random.randint(1,10)
-guess = int(input("Enter the number between 1 and 10: "))
 
-if n == guess:
-    print("Well done")
-else:
-    print("Wrong!")
+while True:
+    guess = int(input("1 dan 10 gacha son kiriting: "))
+    if n == guess:
+        print("Well done 👍")
+        break
