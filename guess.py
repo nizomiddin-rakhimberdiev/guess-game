ee215db39b0b20252cf988c716730067159fac54
nizomiddin-rakhimberdiev <nizomiddinoff@mail.ru>
2023-11-17 11:50:32 +0500

    o'yinga limit qo'shildi

--- guess.py
+++ guess.py
@@ -2,8 +2,17 @@ import random
 
 n = random.randint(1,10)
 
+x = 0
+
 while True:
     guess = int(input("1 dan 10 gacha son kiriting: "))
     if n == guess:
         print("Well done 👍")
         break
+    
+    x += 1
+
+    if x == 3:
+        print("Yutqazgizgiz ☹️:")
+        break
+    
